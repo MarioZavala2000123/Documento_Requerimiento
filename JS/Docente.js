5054f6e91d5aef6bc0fd82dfc5e59dfce735a59f
Mario Zavala <mrzavalat@alumno.uspg.edu.gt>
2022-05-27 21:59:32 -0500

    Fallo en leer tabla

---
+++ JS/Docente.js
@@ -0,0 +1,121 @@
+const url = 'http://147.182.187.126/attendance/docente'
+const opts = { crossDomain: true}
+
+const iddocente = document.getElementById('idDocente')
+const txtDescripcionActividad = document.getElementById('txtDescripcionActividad')
+const myTableActividad = document.getElementById('myTableActividad')
+const txtDesc = document.getElementById('txtDesc')
+const txtIdUpdate = document.getElementById('txtIdUpdate')
+const txtCre = document.getElementById('txtCre')
+const table = document.getElementById('testBodyActividad')
+
+function fetchActividades(){
+    fetch(url + "/" + iddocente.value )
+    .then(response => response.json())
+    .then(data => loadTableData(data))
+  }
+
+function fetchSaveActividades(){
+    var nombresD = txtDescripcionActividad.value
+    console.log(nombresD)
+    
+  var data = { 
+              nombre: nombresD,
+          //    creditos: 7
+        }
+
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+      })
+      .then(response => response.json()) 
+      .then(json => console.log(json))
+      .catch(err => console.log(err))
+
+      txtDescripcionActividad.value = ""
+      fetchActividades()
+    
+}
+
+function fetchUpdateActividades(){
+  console.log($("#txtIdUpdate").val())
+
+  var des = $("#txtDesc").val()
+  var id = $("#txtIdUpdate").val()
+  var cre = $("#txtCre").val()
+  console.log(id)
+  console.log(des)
+  fetch(url, {
+      method: "PUT",
+      body: JSON.stringify({"idDocente" : id , "nombre" : des , "creditos":cre }),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
+
+  
+  
+}
+
+  function loadTableData(items){
+    table.innerHTML = ''
+    if(Object.keys(items).length < 4){
+      let row = table.insertRow();
+      let idActividad = row.insertCell(0)
+      idActividad.innerHTML = items.idDocente
+      let descripcion = row.insertCell(1)
+      descripcion.innerHTML = items.nombres
+      let apellidos = row.insertCell(2)
+      apellidos.innerHTML = items.apellidos
+      let edit = row.insertCell(3)
+      edit.innerHTML = '<a class="btn btn-primary btn-lg"  role="button" id="btnfind4" onclick="openModal() ">Select</a>'
+
+    }else {
+      for(item in items){
+        let row = table.insertRow();
+        
+        let idActividad = row.insertCell(0)
+        idActividad.innerHTML = items[item].idDocente
+        let descripcion = row.insertCell(1)
+        descripcion.innerHTML = items[item].nombres
+        let apellidos = row.insertCell(2)
+        apellidos.innerHTML = items[item].apellidos
+        let edit = row.insertCell(3)
+        edit.innerHTML = '<a class="btn btn-primary btn-lg"  role="button" id="btnfind4" onclick="openModal()">Select</a>'
+    }
+    }
+    
+    
+}
+
+function openModal(){
+  console.log('entra en función')
+  var table = document.getElementById('testBodyActividad');
+  console.log(table)
+  var cells = table.getElementsByTagName('td');
+  for (var i = 0; i < cells.length; i++) {
+    // Take each cell
+    var cell = cells[i];
+    // do something on onclick event for cell
+    cell.onclick = function () {
+        // Get the row id where the cell exists
+        
+        var rowId = this.parentNode.rowIndex-1;
+        console.log("print row id")
+        console.log(rowId)
+        var rowSelected = table.getElementsByTagName('tr')[rowId];
+        console.log(rowSelected)
+        console.log(rowSelected.cells[0].innerHTML)
+
+      
+        $("#txtIdUpdate").val(rowSelected.cells[0].innerHTML)
+        $("#txtDesc").val(rowSelected.cells[1].innerHTML)
+        $("#txtCre").val(rowSelected.cells[2].innerHTML)
+        rowSelected.className += " selected";
+             
+    }
+}
+   $("#exampleModal").modal()
+}
